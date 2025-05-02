1ebcb4713be3cec5ece45103809094b1aaec1cdc
Mustafa Saifee <mustafa.saifee@undp.org>
2025-05-02 18:53:46 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,30 +6,9 @@ import eslint from 'vite-plugin-eslint';
 import dts from 'vite-plugin-dts';
 import tailwindcss from '@tailwindcss/vite';
 
-function removeFonts() {
-  return {
-    name: 'remove-fonts',
-    enforce: 'post', // Ensure it runs after Vite's default asset handling
-    generateBundle(_options, bundle) {
-      for (const name in bundle) {
-        if (/\.(woff|woff2|eot|ttf|otf)$/i.test(name)) {
-          delete bundle[name];
-        }
-      }
-    },
-  };
-}
-
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const isProduction = command === 'build';
-
+export default defineConfig(() => {
   const plugins = [dts({ rollupTypes: true }), react(), eslint(), tailwindcss()];
-
-  // Only include removeFonts plugin when building for production
-  if (isProduction) {
-    plugins.push(removeFonts());
-  }
   return {
     plugins,
     build: {
