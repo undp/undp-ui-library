bce52212a438fed3b8f0c5239f011ecd329d8ac6
Mustafa Saifee <mustafa.saifee@undp.org>
2025-06-23 00:28:47 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,41 @@
+import { A, H3, P } from '@/index';
 import './index.css';
-
-import {
-  TableCaption,
-  TableHeader,
-  TableRow,
-  TableHead,
-  TableBody,
-  TableCell,
-  Table,
-} from './components/ui/table';
 
 function App() {
   return (
-    <>
-      <Table>
-        <TableCaption>A list of your recent invoices.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className='w-[100px]'>Invoice</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Method</TableHead>
-            <TableHead className='text-right'>Amount</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableCell className='font-medium'>INV001</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell className='text-right'>$250.00</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </>
+    <div
+      style={{
+        height: '90vh',
+        maxWidth: '712px',
+        margin: '0 auto',
+        padding: '2rem',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <img width='56' alt='undp-logo' src='/undp-logo-blue.svg' />
+      <H3 style={{ textAlign: 'center', paddingTop: '24px' }}>UNDP Design System for React</H3>
+      <P style={{ textAlign: 'center' }}>
+        This design system for react, developed by the United Nations Development Programme, offers
+        various UI components. You can access the documentation{' '}
+        <A href='https://react.design.undp.org/' target='_blank' rel='noreferrer'>
+          here
+        </A>
+        .
+      </P>
+      <P
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        For any feedback or inquiries, please feel free to reach out to us at{' '}
+        <A href='mailto:data@undp.org' target='_blank' rel='noreferrer'>
+          data@undp.org
+        </A>
+      </P>
+    </div>
   );
 }
 
